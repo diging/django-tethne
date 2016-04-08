720dea9fcb34042488ce75573264d3085634f93f
Abhishek Singh <aosingh@linus.dhcp.asu.edu>
2016-04-08 16:14:01 -0700

    Changed the database Engine

--- django-tethne/settings.py
+++ django-tethne/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'django-tethne.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tethne',
         'USER':'tethneuser',
         'PASSWORD':'password',
