8eff7186e12ed5a4ec42cce0b176c8a2e77b3452
Erick Peirson <erick.peirson@asu.edu>
2016-07-18 09:47:49 -0400

    config

--- tethneweb/wsgi.py
+++ tethneweb/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+try:
+    sys.path.append('/etc')
+    from tethne_conf import DJANGO_SECRET_KEY
+    os.environ['DJANGO_SECRET_KEY'] = DJANGO_SECRET_KEY
+except ImportError:
+    pass
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tethneweb.settings")
 
