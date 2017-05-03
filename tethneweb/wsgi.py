67fcd6c67e56d958dcbb840eba1f0149186d20e4
Erick Peirson <erick.peirson@asu.edu>
2017-05-03 09:08:25 -0400

    added github auth

--- tethneweb/wsgi.py
+++ tethneweb/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for django_tethne project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os, sys
 
 from django.core.wsgi import get_wsgi_application
